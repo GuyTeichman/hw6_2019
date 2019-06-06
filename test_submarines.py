c69475bf2bc26e3a436cb76cb15f2e482f90aa6b
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2019-06-06 11:51:30 +0300

    new test suite

--- test_submarines.py
+++ test_submarines.py
@@ -68,13 +68,13 @@ def test_board_too_small_for_piece_type():
     assert "Board size is too small to accommodate" in str(e.value)
 
 
-
 def test_too_many_pieces_to_fit_in_board():
-    board_size = (5,5)
+    board_size = (5, 5)
     number_of_pieces = {AvailablePieces.Destroyer: 7}
     with pytest.raises(AssertionError) as e:
         myboard = Board(board_size, number_of_pieces)
     assert "Too many pieces of type" in str(e.value)
+
 
 def test_board_creation():
     board_size = (10, 10)
@@ -86,12 +86,41 @@ def test_board_creation():
     assert Board(board_size_2, number_of_pieces_2)
 
 
-def test_board_invalid_input():
-    pass
+def test_board_strike_invalid_input():
+    board_size = (5, 5)
+    number_of_pieces = {AvailablePieces.Destroyer: 2}
+    brd = Board(board_size, number_of_pieces)
+    inputs = ["5,5,1", ("0,0,3"), "0,1", ("0,1,"), "0,4,-1", "h,s,f", "", "0,1,2,2"]
+    for inp in inputs:
+        assert not brd.test_coords_valid(inp)
 
 
-def test_board_nonpositive_index():
-    pass
+def test_sturdy_gamepiece_damaged_not_destroyed():
+    dst = Destroyer((0, 0, 1), 0, "board", False)
+    assert not dst.destroyed
+    dst.pixels[0].hit()
+    assert dst.pixels[0].damaged
+    assert not dst.pixels[1].damaged
+    assert not dst.destroyed
+
+
+def test_sturdy_gamepiece_destroyed():
+    dst = Destroyer((0, 0, 1), 0, "board", False)
+    assert not dst.destroyed
+    for pxl in dst.pixels:
+        pxl.hit()
+    for pxl in dst.pixels:
+        assert pxl.damaged
+    assert dst.destroyed
+
+
+def test_unsturdy_gamepiece_destroyed():
+    sub = Submarine((0, 0, 0), 0, "board", False)
+    assert not sub.destroyed
+    sub.pixels[0].hit()
+    for pxl in sub.pixels:
+        assert pxl.damaged
+    assert sub.destroyed
 
 
 def test_multiple_generals():
@@ -99,3 +128,19 @@ def test_multiple_generals():
     number_of_pieces = {AvailablePieces.General: 2}
     with pytest.raises(AssertionError):
         board = Board(board_size, number_of_pieces)
+
+
+def test_general_game_over():
+    pass
+
+
+def test_game_over():
+    board_size = (1, 1)
+    number_of_pieces = {AvailablePieces.General: 1}
+    brd = Board(board_size, number_of_pieces)
+    for z in range(3):
+        brd.strike(0, 0, z)
+    for piece in brd.pieces:
+        assert piece.destroyed
+        for pxl in piece.pixels:
+            assert pxl.damaged
