d0739dffb352ca70d114be3b6da10d47c33bb364
GuyTeichman <48219633+GuyTeichman@users.noreply.github.com>
2019-06-06 11:32:31 +0300

    added function descriptions, removed unnecessary imports.

--- submarines_3d.py
+++ submarines_3d.py
@@ -1,6 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from collections import namedtuple as tpl
 from enum import Enum
 
 
@@ -40,9 +38,14 @@ class GamePiece:
         return f"{type(self).__name__}{self.idx}"
 
     def change_orientation(self):
+        """if flip == True, this function is called and transposes the game piece. """
         self.shape = self.shape.T
 
     def hit(self):
+        """This function triggers when a game piece is reported to be hit (normally triggered by a child Pixel object).
+        If the game piece is sturdy (like Destroyer) it tests to see whether it has any undamaged pixels left, and only
+        if it does then it will trigger its own death. If it is not sturdy, it will change the status of all of its
+        Pixels to be 'damaged', and will then automatically trigger its own death. """
         if self.is_sturdy:
             print("Signal.HIT")
             for pxl in self.pixels:
@@ -55,6 +58,8 @@ class GamePiece:
         self.kill()
 
     def kill(self):
+        """This function triggers when the game piece is destroyed. It changes the piece's status to 'destroyed', and
+        prints to the player. """
         assert not self.destroyed
         self.destroyed = True
         print("Signal.KILL")
@@ -62,7 +67,8 @@ class GamePiece:
 
 
 class Pixel:
-    """A single 'pixel' of a GamePiece object. """
+    """A single 'pixel' of a GamePiece object. These instances are initiated automatically when a game piece is created,
+    and are then placed in the game board."""
     damaged = False
 
     def __init__(self, game_piece: GamePiece, loc: tuple):
@@ -70,15 +76,20 @@ class Pixel:
         self.loc = loc
 
     def __repr__(self):
+        if self.damaged:
+            return "X"
         return f"{type(self.game_piece).__name__}{self.game_piece.idx}"
 
     def __str__(self):
         return f"Pixel of game piece {type(self.game_piece).__name__}{self.game_piece.idx} in coordinates {self.loc}"
 
     def is_damaged(self):
+        """returns the 'damaged' status of the pixel (whether is was hit already or not)"""
         return self.damaged
 
     def hit(self):
+        """Triggers when a Pixel gets hit. Changes its 'damaged' status,
+        and calls its parent gamepiece's 'hit' function. """
         assert not self.damaged
         self.damaged = True
         self.game_piece.hit()
@@ -123,11 +134,19 @@ class General(GamePiece):
         self.z = self.tl[2]
 
     def kill(self):
+        """This function triggers when the General dies. It triggers the death of all other game pieces on the board."""
         for piece in self.board.pieces:
             piece.destroyed = True
 
 
 class Board:
+    """An instance of a game board. Has a player ID (normally 1 or 2), size (rows and columns, with the 3rd dimension
+    automatically set to 3); has a 'board' property which is a numpy matrix containing Pixel objects of game pieces;
+    has a 'pieces' property which contains all of the game pieces present on the board; and 'num_pieces' which
+    specifies how many instances of each game piece should be on the board. The board attempts to automatically place
+    all of the requested pieces on the board. If it fails due to a board size which is too small, or number of pieces
+    which is too big, it will raise an error message. """
+
     def __init__(self, size: tuple, number_of_pieces: dict, player_id: int = 0):
         assert isinstance(player_id, int)
         self.player_id = player_id
@@ -160,6 +179,9 @@ class Board:
         return f"Deep: \n{self.board[:, :, 0]} \nSea-level: \n{self.board[:, :, 1]} \nAir: \n{self.board[:, :, 2]}"
 
     def gen_coord(self, piece_type: GamePiece, z: int, flip: bool):
+        """A function used to generate all possible 'top-left coordinates' for a given piece with the given board size
+        in a random order. If it finds a suitable coordinate it returns it, and the Board instance may continue to
+        place pieces. If no suitable coordinate is found, an error is raised. """
         used = set()
         try:
             piece_rows, piece_cols = np.shape(piece_type.shape)
@@ -193,8 +215,9 @@ class Board:
         raise AssertionError(f"Too many pieces of type {piece_type} for specified board size. "
                              "Pieces cannot be placed without overlap or going out-of-bound.")
 
-    def strike(self, coords_str):
-        x, y, z = eval(coords_str)
+    def strike(self, x: int, y: int, z: int):
+        """This function takes a coordinate input, and checks whether this coordinate is a 'hit' (targets an undamaged
+        Pixel), or a 'miss'. If a hit, it checks whether the game is over using the 'check_game_over()' function. """
         if self.board[x, y, z] == 0 or (isinstance(self.board[x, y, z], Pixel) and self.board[x, y, z].is_damaged()):
             print("Signal.MISS")
             game_over = False
@@ -203,7 +226,9 @@ class Board:
             game_over = self.check_game_over()
         return game_over
 
-    def test_coords_valid(self, coords_str):
+    def test_coords_valid(self, coords_str: str):
+        """This function receives an input string from a player, and tests whether it is a valid coordinate input:
+        it must be composed of 3 integers exactly, which are non-negative, and do not exceed the board's boundaries. """
         try:
             x, y, z = eval(coords_str)
             assert isinstance(x, int) and isinstance(y, int) and isinstance(z, int)
@@ -215,6 +240,7 @@ class Board:
         return coords_accepted
 
     def check_game_over(self):
+        """This function checks whether all pieces on the board are destroyed. If so, it returns True."""
         for piece in self.pieces:
             if not piece.destroyed:
                 return False
@@ -223,6 +249,7 @@ class Board:
 
 
 def start():
+    """This function triggers the beginning of the game. """
     boards = [Board(board_size, number_of_game_pieces, 1), Board(board_size, number_of_game_pieces, 2)]
     gameover = False
     quitgame = False
@@ -245,13 +272,16 @@ def start():
         if quitgame:
             print("Quitting game")
             break
-        gameover = boards[i].strike(inp)
-        if gameover:
-            print(f"Game over, player #{boards[(i+1)%2].player_id} won!")
-        i = (i + 1) % 2
+        x, y, z = eval(inp)
+    gameover = boards[i].strike(x, y, z)
+    if gameover:
+        print(f"Game over, player #{boards[(i + 1) % 2].player_id} won!")
+    i = (i + 1) % 2
 
 
 class AvailablePieces(Enum):
+    """An enumerator containing all of the game pieces available in the game. Any new pieces added to the game should
+    be added here as well. """
     Submarine = Submarine
     Destroyer = Destroyer
     Jet = Jet
